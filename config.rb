66760f7667894c32d7e6de91d7e9a10dc2572dc8
konskiy.bolt <konskiy.bolt@mail.ru>
2015-05-08 01:10:00 +0400

    new files

--- config.rb
+++ config.rb
@@ -7,6 +7,7 @@ sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
 
+
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :expanded
 
@@ -16,6 +17,7 @@ relative_assets = true
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
 
+asset_cache_buster :none
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
